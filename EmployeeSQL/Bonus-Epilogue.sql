f35aeb5659a3c2e4e15ebc34b3b4dd367a4594fd
abpuccini <b.atcharaporn@gmail.com>
2020-10-30 11:54:28 -0400

    Q7 and Bonus(Update Employee info)

--- EmployeeSQL/Bonus-Epilogue.sql
+++ EmployeeSQL/Bonus-Epilogue.sql
@@ -1,7 +1,16 @@
 -- Epilogue
 
+-- Display employee#499942
+SELECT * FROM employees
+WHERE emp_no = 499942;
+
+-- Update employee info to my name and hire_date
+UPDATE employees
+SET (last_name, first_name, hire_date) = ('Puccini', 'Atcharaporn', '2020-11-04')
+WHERE emp_no = 499942;
+
 -- Select columns to display
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name, s.salary
+SELECT e.emp_no, e.last_name, e.first_name, e.hire_date, d.dept_name
 -- Get employees table as the first table to join other tables
 FROM employees AS e
 	-- Join dept_emp table to get dept_no to link to department name in department table
@@ -10,8 +19,8 @@ FROM employees AS e
 		-- Join department table to have department name
 		JOIN department AS d
 		ON de.dept_no = d.dept_no
-			-- Join salaries table to have salary info
-			JOIN salaries AS s
-			ON e.emp_no = s.emp_no
 -- Apply condition to filter only employee#499942
 WHERE e.emp_no = 499942;
+
+
+
